d6c93e2d3ffd5d8ef5ebb2a551d91c493aa5a05d
JCT <JCSTwo@gmail.com>
2019-08-03 09:21:26 +0800

    Updated Bootloader/ISP_I2C sample  - PIN 5, 4 = I2C0_SCL, I2C0_SDA  - PIN 1, 2 = I2C1_SCL, I2C1_SDA  - Both I2C0 & I2C1 are enabled, please don't use    these two ports simultaneously.

---
+++ NUC123BSPv3/MyLibrary/HAL_Driver/nb_hal_sys_init.h
@@ -0,0 +1,67 @@
+#ifndef __HAL_SYS_INIT_H__
+#define __HAL_SYS_INIT_H__
+
+#include "NUC123.h"
+
+__STATIC_INLINE void SYS_Init_72MHZ(void)
+{
+    /* Enable XT1_OUT (PF.0) and XT1_IN (PF.1) */
+    SYS->GPF_MFP |= SYS_GPF_MFP_PF0_XT1_OUT | SYS_GPF_MFP_PF1_XT1_IN;
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Enable Internal RC 22.1184 MHz clock */
+    /* Enable external XTAL 12 MHz clock */
+    CLK->PWRCON |= (CLK_PWRCON_OSC22M_EN_Msk | CLK_PWRCON_XTL12M_EN_Msk);
+
+    /* Waiting for external XTAL clock ready */
+    while (!(CLK->CLKSTATUS & CLK_PWRCON_XTL12M_EN_Msk));
+
+    /* Set core clock as CLK_PLLCON_144MHz_HXT from PLL */
+    CLK->PLLCON = CLK_PLLCON_144MHz_HXT;
+
+    while (!(CLK->CLKSTATUS & CLK_CLKSTATUS_PLL_STB_Msk));
+
+    CLK->CLKDIV = (CLK->CLKDIV & (~CLK_CLKDIV_HCLK_N_Msk)) | CLK_CLKDIV_HCLK(2);
+    CLK->CLKSEL0 &= (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLK_S_Msk)) | CLK_CLKSEL0_HCLK_S_PLL;
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate PllClock, SystemCoreClock and CycylesPerUs automatically. */
+    //SystemCoreClockUpdate();
+    PllClock        = 144000000;
+    SystemCoreClock = 72000000;
+    CyclesPerUs     = 72;
+    /* Enable module clock */
+    CLK->AHBCLK |= CLK_AHBCLK_ISP_EN_Msk;   // (1ul << 2)
+}
+
+__STATIC_INLINE void SYS_Init_USBD(void)
+{
+    CLK->CLKDIV = (CLK->CLKDIV & (~CLK_CLKDIV_USB_N_Msk)) | CLK_CLKDIV_USB(3);
+    CLK->APBCLK |= CLK_APBCLK_USBD_EN_Msk;
+}
+
+// PIN 5, 4 = I2C0_SCL, I2C0_SDA
+__STATIC_INLINE void SYS_Init_I2C0(void)
+{
+    /* Enable I2C controller */
+    CLK->APBCLK |= CLK_APBCLK_I2C0_EN_Msk;
+    /* Set GPF multi-function pins for I2C0 SDA and SCL */
+    SYS->GPF_MFP &= ~(SYS_GPF_MFP_PF2_Msk | SYS_GPF_MFP_PF3_Msk);
+    SYS->GPF_MFP |= (SYS_GPF_MFP_PF2_I2C0_SDA | SYS_GPF_MFP_PF3_I2C0_SCL);
+    SYS->ALT_MFP1 &= ~(SYS_ALT_MFP1_PF2_Msk | SYS_ALT_MFP1_PF3_Msk);
+    SYS->ALT_MFP1 |= (SYS_ALT_MFP1_PF2_I2C0_SDA | SYS_ALT_MFP1_PF3_I2C0_SCL);
+}
+
+// PIN 1, 2 = I2C1_SCL, I2C1_SDA
+__STATIC_INLINE void SYS_Init_I2C1(void)
+{
+    /* Enable I2C controller */
+    CLK->APBCLK |= CLK_APBCLK_I2C1_EN_Msk;
+    /* Set GPA multi-function pins for I2C1 SDA and SCL */
+    SYS->GPA_MFP &= ~(SYS_GPA_MFP_PA10_Msk | SYS_GPA_MFP_PA11_Msk);
+    SYS->ALT_MFP &= ~(SYS_ALT_MFP_PA10_Msk | SYS_ALT_MFP_PA11_Msk);
+    SYS->GPA_MFP |= (SYS_GPA_MFP_PA10_I2C1_SDA | SYS_GPA_MFP_PA11_I2C1_SCL);
+    SYS->ALT_MFP |= (SYS_ALT_MFP_PA10_I2C1_SDA | SYS_ALT_MFP_PA11_I2C1_SCL);
+}
+
+#endif  /* __HAL_SYS_INIT_H__ */
--- NUC123BSPv3/SampleCode/Bootloader/i2c_transfer.c
+++ NUC123BSPv3/SampleCode/Bootloader/i2c_transfer.c
@@ -1,118 +1,142 @@
 #include <stdio.h>
-#include <string.h>
-#include "i2c_transfer.h"
 #include "targetdev.h"
 
-__align(4) uint8_t I2C_rcvbuf[MAX_PKT_SIZE] = {0};
-__align(4) uint8_t I2C_sendbuf[MAX_PKT_SIZE];
-__align(4) uint8_t I2C_aprom_buf[PAGE_SIZE];
-
-uint8_t volatile bI2CDataReady = 0;
-uint8_t volatile bufhead = 0;
-
-volatile uint8_t I2C_Read = FALSE;
-uint8_t Send_Cnt = 0;
-uint8_t Rcv_Cnt = 0;
-volatile uint8_t EndFlag = 0;
-
-uint8_t I2C_Slave_Addr = 0x36;
-
-void I2C_SlaveTRx(uint32_t u32Status);
 /*---------------------------------------------------------------------------------------------------------*/
-/* INTSTS to handle I2C Channel 0 interrupt event                                                            */
+/* Global variables                                                                                        */
 /*---------------------------------------------------------------------------------------------------------*/
-void I2C0_IRQHandler(void)
+uint8_t i2c_rcvbuf[64];
+volatile uint8_t bI2cDataReady;
+
+volatile uint8_t g_u8DeviceAddr = 0x60;
+volatile uint8_t g_u8SlvDataLen;
+
+__STATIC_INLINE void I2C_SlaveTRx(I2C_T *i2c, uint32_t u32Status);
+
+void I2C_Init(I2C_T *i2c, uint32_t u32BusClock)
+{
+    IRQn_Type IRQn;
+
+    /* Reset I2C Controller */
+    if ((uint32_t)i2c == I2C0_BASE) {
+        SYS->IPRSTC2 |= SYS_IPRSTC2_I2C0_RST_Msk;
+        SYS->IPRSTC2 &= ~SYS_IPRSTC2_I2C0_RST_Msk;
+        IRQn = I2C0_IRQn;
+    } else if ((uint32_t)i2c == I2C1_BASE) {
+        SYS->IPRSTC2 |= SYS_IPRSTC2_I2C1_RST_Msk;
+        SYS->IPRSTC2 &= ~SYS_IPRSTC2_I2C1_RST_Msk;
+        IRQn = I2C1_IRQn;
+    }
+
+    /* Enable I2C Controller */
+    i2c->I2CON |= I2C_I2CON_ENS1_Msk;
+    /* I2C bus clock 100K divider setting, I2CLK = PCLK/(100K*4)-1 */
+    i2c->I2CLK = (uint32_t)(((SystemCoreClock * 10) / (u32BusClock * 4) + 5) / 10 - 1); /* Compute proper divider for I2C clock */
+    /* Set I2C Slave Addresses */
+    i2c->I2CADDR0 = (i2c->I2CADDR0 & ~I2C_I2CADDR_I2CADDR_Msk) | (g_u8DeviceAddr << I2C_I2CADDR_I2CADDR_Pos);
+    /* Enable I2C interrupt and set corresponding NVIC bit */
+    i2c->I2CON |= I2C_I2CON_EI_Msk;
+    NVIC_EnableIRQ(IRQn);
+    /* I2C enter no address SLV mode */
+    I2C_SET_CONTROL_REG(i2c, I2C_I2CON_SI_AA);
+}
+
+
+#define STATUS              I2CSTATUS
+#define TOCTL               I2CTOC
+#define I2C_TOCTL_TOIF_Msk  I2C_I2CTOC_TIF_Msk
+#define I2C_CTL_SI_AA       I2C_I2CON_SI_AA
+#define I2C_CTL_SI          I2C_I2CON_SI
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  I2C IRQ Handler                                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+__STATIC_INLINE void I2C_IRQHandler(I2C_T *i2c)
 {
     uint32_t u32Status;
-    u32Status = I2C_GET_STATUS(I2C0);
+    u32Status = I2C_GET_STATUS(i2c);
 
-    if (I2C_GET_TIMEOUT_FLAG(I2C0)) {
-        /* Clear I2C0 Timeout Flag */
-        I2C0->I2CTOC |= I2C_I2CTOC_TIF_Msk;
+    if (I2C_GET_TIMEOUT_FLAG(i2c)) {
+        /* Clear I2C Timeout Flag */
+        i2c->I2CTOC |= I2C_I2CTOC_TIF_Msk;
     } else {
-        I2C_SlaveTRx(u32Status);
+        I2C_SlaveTRx(i2c, u32Status);
     }
+}
+
+void I2C0_IRQHandler()
+{
+    I2C_IRQHandler(I2C0);
+}
+
+void I2C1_IRQHandler()
+{
+    I2C_IRQHandler(I2C1);
 }
 
 /*---------------------------------------------------------------------------------------------------------*/
 /*  I2C TRx Callback Function                                                                               */
 /*---------------------------------------------------------------------------------------------------------*/
-void I2C_SlaveTRx(uint32_t u32Status)
+void I2C_SlaveTRx(I2C_T *i2c, uint32_t u32Status)
 {
-    /* SLA + R */
-    if ((u32Status == 0xA8) || (u32Status == 0xB0)) {	/* Own SLA+R has been received and ACK/NACK has been return */
-        I2C_Read = FALSE;
-        Send_Cnt = 0;
-        I2C0->I2CDAT = I2C_sendbuf[Send_Cnt++];
-        I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
-    } else if (u32Status == 0xB8) {										/* Data byte has been transmitted and ACK has been received */
-        if (Send_Cnt < MAX_PKT_SIZE) {
-            I2C0->I2CDAT = I2C_sendbuf[Send_Cnt++];
-            I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
+    uint8_t u8data;
+
+    if (u32Status == 0x60) {                    /* Own SLA+W has been receive; ACK has been return */
+        bI2cDataReady = 0;
+        g_u8SlvDataLen = 0;
+        I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
+    } else if (u32Status == 0x80)                 /* Previously address with own SLA address
+                                                   Data has been received; ACK has been returned*/
+    {
+        i2c_rcvbuf[g_u8SlvDataLen] = I2C_GET_DATA(i2c);
+        g_u8SlvDataLen++;
+        g_u8SlvDataLen &= 0x3F;
+        bI2cDataReady = (g_u8SlvDataLen == 0);
+
+        if (g_u8SlvDataLen == 0x3F) {
+            I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
         } else {
-            I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
-            EndFlag = 1;
+            I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
         }
-    } else if (u32Status == 0xC0) {    /* Data byte or last data in I2CDAT has been transmitted Not ACK has been received */
-        I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
-        EndFlag = 1;
-    }
-    /* SLA + W */
-    else if ((u32Status == 0x60) || (u32Status == 0x68)) {	/* Own SLA+W has been received and ACK has been return */
-        I2C_Read = TRUE;
-        Rcv_Cnt = 0;
-        bI2CDataReady = TRUE;
-        I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
-    } else if (u32Status == 0x80) {													/* Data byte has been received and ACK has been received */
-        if (Rcv_Cnt < (MAX_PKT_SIZE - 1)) {
-            I2C_rcvbuf[Rcv_Cnt++] = I2C0->I2CDAT;
-            I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
+    } else if (u32Status == 0xA8) {             /* Own SLA+R has been receive; ACK has been return */
+        g_u8SlvDataLen = 0;
+        u8data = response_buff[g_u8SlvDataLen];
+        I2C_SET_DATA(i2c, u8data);
+        g_u8SlvDataLen++;
+        I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
+    } else if (u32Status == 0xB8) {
+        u8data = response_buff[g_u8SlvDataLen];
+        I2C_SET_DATA(i2c, u8data);
+        g_u8SlvDataLen++;
+        g_u8SlvDataLen &= 0x3F;
+
+        if (g_u8SlvDataLen == 0x00) {
+            I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
         } else {
-            I2C_rcvbuf[Rcv_Cnt++] = I2C0->I2CDAT;
-            I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
-            EndFlag = 1;
+            I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
         }
-    } else if (u32Status == 0xA0) {					/* A STOP or repeated START has been received while still addressed as SLA/REC */
-        I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
+    } else if (u32Status == 0xC8) {
+        I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
+    } else if (u32Status == 0xC0)                 /* Data byte or last data in I2CDAT has been transmitted
+                                                   Not ACK has been received */
+    {
+        I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
+    } else if (u32Status == 0x88)                 /* Previously addressed with own SLA address; NOT ACK has
+                                                   been returned */
+    {
+        i2c_rcvbuf[g_u8SlvDataLen] = I2C_GET_DATA(i2c);
+        g_u8SlvDataLen++;
+        bI2cDataReady = (g_u8SlvDataLen == 64);
+        g_u8SlvDataLen = 0;
+        I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
+    } else if (u32Status == 0xA0)                 /* A STOP or repeated START has been received while still
+                                                   addressed as Slave/Receiver*/
+    {
+        g_u8SlvDataLen = 0;
+        I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI_AA);
     } else {
-        I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
-
-        if (I2C_Read) {
-            bI2CDataReady = FALSE;
-        }
-
-        EndFlag = 1;
+        /* TO DO */
+        // printf("Status 0x%x is NOT processed\n", u32Status);
     }
 }
 
-void I2C_Init()
-{
-    /* Enable I2C controller */
-    CLK->APBCLK |= CLK_APBCLK_I2C0_EN_Msk;
-    /* Set GPF multi-function pins for I2C0 SDA and SCL */
-    SYS->GPF_MFP |= (SYS_GPF_MFP_PF2_I2C0_SDA | SYS_GPF_MFP_PF3_I2C0_SCL);
-    SYS->ALT_MFP1 &= ~(SYS_ALT_MFP1_PF2_Msk | SYS_ALT_MFP1_PF3_Msk);
-    SYS->ALT_MFP1 |= (SYS_ALT_MFP1_PF2_I2C0_SDA | SYS_ALT_MFP1_PF3_I2C0_SCL);
-    /* Reset I2C */
-    SYS->IPRSTC2 |=  SYS_IPRSTC2_I2C0_RST_Msk;
-    SYS->IPRSTC2 &= ~SYS_IPRSTC2_I2C0_RST_Msk;
-    /* Enable I2C Controller */
-    I2C0->I2CON |= I2C_I2CON_ENS1_Msk;
-    /* I2C bus clock 100K divider setting, I2CLK = PCLK/(100K*4)-1 */
-    I2C0->I2CLK = (uint32_t)(((SystemCoreClock * 10) / (100000 * 4) + 5) / 10 - 1); /* Compute proper divider for I2C clock */
-    /* Set I2C0 Slave Addresses */
-    /* Slave Address : 0x36 */
-    I2C0->I2CADDR0 = (I2C0->I2CADDR0 & ~I2C_I2CADDR_I2CADDR_Msk) | (I2C_Slave_Addr << I2C_I2CADDR_I2CADDR_Pos);
-    /* Enable I2C0 interrupt and set corresponding NVIC bit */
-    I2C0->I2CON |= I2C_I2CON_EI_Msk;
-    NVIC_EnableIRQ(I2C0_IRQn);
-}
 
-void I2C_SlaveRcvSendData()
-{
-    EndFlag = 0;
-    /* I2C enter no address SLV mode */
-    I2C_SET_CONTROL_REG(I2C0, I2C_I2CON_SI_AA);
-
-    while ((DetectPin == 0) && (EndFlag == 0));
-}
